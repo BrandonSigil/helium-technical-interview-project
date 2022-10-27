76ac3c49c681b818d4f7d55f84ddf6631c3b55b6
Brandon <Brandonwirth@outlook.com>
2022-10-27 09:57:35 -0700

    ChoicelistProp Types setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,111 @@
 import './App.sass';
 
-import React from 'react';
+import React, { useState } from 'react';
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import Grid from './GRID.svg';
-import ChoiceList from './components/ChoiceList';
+import { ChoiceList } from './components/ChoiceList';
+import { states } from './data/stateOptions';
 
-export default function MultipleSelect() {
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: '',
+      },
+    }),
+  },
+});
+
+export default function App() {
+  const [listAValue, setListAValue] = useState<string>('');
+  const [listBValue, setListBValue] = useState<string[]>([]);
+  const [listCValue, setListCValue] = useState<string>('');
+  const [listDValue, setListDValue] = useState<string[]>([]);
+
   return (
-    <div className="body-container">
-      <header>
-        <div>
-          <h3>Choosey.</h3>
-        </div>
-        <div className="pill-chooser"><p>the original choosey</p></div>
+    <ChakraProvider resetCSS={false} theme={theme}>
+      <div className="body-container">
+        <header>
+          <div>
+            <h3>Choosey.</h3>
+          </div>
+          {/* <div className="pill-chooser"><p>the original choosey</p></div>
         <div className="pill-chooser"><p>multi-choosey</p></div>
         <div className="pill-chooser"><p>choosey search</p></div>
-      </header>
-      <div className="gradient-container">  </div>
-      <div className="hero-container">
-        <img className="hero__grid" alt="grid-svg-design" src={Grid} />
-        <h2 className="hero__sub-head">In a world with so many options, it's ok to be:</h2>
-        <h1>CHOOSEY</h1>
-      </div>
-      <div className="choicelist-container">
-        <div className="choosey-title">
-          <ArrowCircleLeftOutlinedIcon />
-          <h2>The Original Choosey</h2>
-          <ArrowCircleRightOutlinedIcon />
+        <div className="pill-chooser"><p>multi-choosey search extreme</p></div> */}
+        </header>
+        <div className="gradient-container">  </div>
+        <div className="hero-container">
+          <img className="hero__grid" alt="grid-svg-design" src={Grid} />
+          <h2 className="hero__sub-head">In a world with so many options, it's ok to be:</h2>
+          <h1>CHOOSEY</h1>
         </div>
-        <div className="choosey-description">
-          <p> All gas, no brakes. Pick from multiple choices. </p>
-        </div>
-        <div className="choice-list-carousel" />
-      </div>
-      {/* <div className="description">
-        <div>
-          <p>
-          At Choosey, fashion isn't just a purchase you make or the clothes adorning your bod.
-          It's about a lifestyle. Where will you be when Choosey Choicelist comes back?
-          Hopefully drippin' in our $800 t-shirt.
-          </p>
-        </div>
+        <div className="choicelist-container">
+          <div className="choosey-title">
+            <ArrowCircleLeftOutlinedIcon />
+            <h2>The Original Choosey</h2>
+            <ArrowCircleRightOutlinedIcon />
+          </div>
+          <div className="choosey-description">
+            <p> All gas, no brakes. Pick from multiple choices. </p>
+          </div>
+          <div className="choice-list-carousel">
+            <div>
+              <ChoiceList
+                label="Single option"
+                selected={listAValue}
+                options={[
+                  { label: 'Option A', value: 'a' },
+                  { label: 'Option B', value: 'b' },
+                  { label: 'Option C', value: 'c' },
+                  { label: 'Option D', value: 'd' },
+                  { label: 'Option E', value: 'e' },
+                ]}
+                onSelect={setListAValue}
+              />
+            </div>
 
+            <div>
+              <ChoiceList
+                label="Multiple options"
+                selected={listBValue}
+                options={[
+                  { label: 'Option A', value: 'a' },
+                  { label: 'Option B', value: 'b' },
+                  { label: 'Option C', value: 'c' },
+                  { label: 'Option D', value: 'd' },
+                  { label: 'Option E', value: 'e' },
+                ]}
+                allowMultiple
+                onSelect={setListBValue}
+              />
+            </div>
+
+            <ChoiceList
+              label="Single option w/ search"
+              autocomplete
+              selected={listCValue}
+              options={states}
+              allowSearch
+              onSelect={setListCValue}
+            />
+
+            <ChoiceList
+              label="Multiple options w/ search"
+              selected={setListDValue}
+              options={states}
+              allowSearch
+              allowMultiple
+              onSelect={listDValue}
+            />
+
+          </div>
+        </div>
+        <footer>#Better2BeChoosey #DontSnoozeOnChoosey</footer>
       </div>
-      <footer>#Better2BeChoosey</footer> */}
-    </div>
+    </ChakraProvider>
+
   );
 }
--- src/components/ChoiceList.tsx
+++ src/components/ChoiceList.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-
 // Implement your ChoiceList component here.
-export default function ChoiceList() {
-  return (
-    <>
+import * as React from 'react';
+import { Select } from '@chakra-ui/react';
 
-    </>
+export type ChoiceListProps = {
+  label: string;
+  selected: React.Dispatch<React.SetStateAction<string>> | React.Dispatch<React.SetStateAction<string[]>> | string | string[];
+  onSelect?: React.Dispatch<React.SetStateAction<string>> | React.Dispatch<React.SetStateAction<string[]>> | string[];
+  options:{
+    label: any;
+    value: string;
+  }[];
+  autocomplete?: boolean;
+  allowSearch?: boolean;
+  allowMultiple?: boolean;
+
+};
+
+export function ChoiceList(props: ChoiceListProps): React.ReactElement {
+  // const renderOptionList = props.options.map((label, value) => {
+  //   <option key={value}>{label}</option>
+  // });
+
+  console.log(props.options.label)
+
+  return (
+    <Select placeholder={props.label }>
+      {/* {props.options.map((label, value) => (
+        <option key={value}>{label}</option>
+      ))} */}
+    </Select>
   );
 }
+
+export default ChoiceList;
